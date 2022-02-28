ab5fa72d9a4c349cc0d06034beed7d05bfd56470
George Stavropoulos <georgestavropoulos0@gmail.com>
2022-02-28 15:11:06 +0000

    added custom_widgets module: qtile

--- dotfiles/qtile/settings/path.py
+++ dotfiles/qtile/settings/path.py
@@ -1,4 +1,5 @@
-from os import path
+from os import path, popen
 
 qtile_path = path.join(path.expanduser('~'), ".config", "qtile")
-scripts_path = path.join(path.expanduser('~'), ".local", "bin")
+scripts_path = popen('echo $SCRIPTS').read().replace('\n', '')
+# scripts_path = path.join(path.expanduser('~'), ".local", "bin")
