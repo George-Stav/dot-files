10797ace041bb7f83fdcf4d8a166147060c6f923
George Stavropoulos <georgestavropoulos0@gmail.com>
2022-05-10 08:52:34 +0100

    picom + new themes + media-notif

--- dotfiles/qtile/autostart.sh
+++ dotfiles/qtile/autostart.sh
@@ -7,9 +7,10 @@
 # optimus-manager --status > /dev/null # for some reason opt-man-qt doesn't run otherwise
 # optimus-manager-qt &
 flameshot &
-$HOME/repos/scripts/scripts/monitor-setup
+picom &
+"$HOME/repos/scripts/scripts/monitor-setup"
 
-eval $(gnome-keyring-daemon --start)
+eval "$(gnome-keyring-daemon --start)"
 # export SSH_AUTH_SOCK
 # xrandr --output "HDMI-1-0" --mode 1920x1080 --rate 144\
 #     --output "DP-1" --primary --mode 1920x1080 --rate 144 --right-of "HDMI-1-0"
