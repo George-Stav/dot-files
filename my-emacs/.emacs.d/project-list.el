36f3ce40e632d509a73ad55265872ad94c6ea11e
George Stavropoulos <georgestavropoulos0@gmail.com>
2023-11-03 22:56:42 +0000

    [wd-sync] change help message

--- my-emacs/.emacs.d/project-list.el
+++ my-emacs/.emacs.d/project-list.el
@@ -1,12 +1,12 @@
 ;;; -*- lisp-data -*-
 (("/home/neeto/dotfiles/")
+ ("/home/neeto/dev/rust/spotiver/")
  ("/home/neeto/notes/")
  ("/home/neeto/shared/marketing-webanalytics/")
  ("/home/neeto/cv/")
  ("/home/neeto/dev/spare-car-parts/")
  ("/home/neeto/shared/ga-entities/")
  ("/home/neeto/dev/rust/genp/")
- ("/home/neeto/dev/rust/spotiver/")
  ("/home/neeto/dev/rust/aoc21/")
  ("/home/neeto/dev/rust/testing/")
  ("/home/neeto/dev/rust/rustlings/")
